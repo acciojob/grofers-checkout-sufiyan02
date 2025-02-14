9722bfbb08cd06c6d5a96b08e50585c33c5e57d9
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-14 12:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,28 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+    let prices = document.querySelectorAll(".price");
+    let total = 0;
+    
+    prices.forEach(price => {
+        total += parseInt(price.innerText);
+    });
+
+	let er = document.getElementById("totalRow");
+	if(er){
+		er.remove();
+	}
   
+    
+    let row = document.createElement("tr");
+    row.id = "totalRow";
+    
+    let cell = document.createElement("td");
+    cell.innerText = `Total Price : ${total}`;
+
+	
+    row.appendChild(cell);
+    document.querySelector("table").appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
